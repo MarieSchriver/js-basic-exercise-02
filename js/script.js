d284aed1273d5d0308ef1a558678ffae4a14d99a
MarieSchriver <eaa24masb@students.eaaa.dk>
2025-01-27 09:26:13 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// Opgave 3: Opret variabler til navn og alder
+let name = "Anders";
+let age = 25;
+
+// Opgave 4: Brug console.log til at vise beskeden
+console.log(`Hi ${name}, you are ${age} years old!`);
+
+// Opgave 5: Tildel nye værdier til variablerne
+name = "Mette";
+age = 30;
+
+// Vis den opdaterede besked med console.log
+console.log(`Hi ${name}, you are ${age} years old!`);
